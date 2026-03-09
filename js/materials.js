55ca013b429496930ff915d20029a8b35a084a3c
Tao Jiang <tao.jiang@timberline.com.au>
2026-03-09 13:55:21 +1100

    feat(configurator): Build advanced two-tier Handle & Top system; dynamically populate Mount and Size

---
+++ js/materials.js
@@ -0,0 +1,363 @@
+const MATERIAL_LIBRARY = {
+  "boston_oak": {
+    "type": "texture",
+    "name": "Boston Oak Woodmatt",
+    "thumbColor": "#D2B48C",
+    "texture": "./assets/textures/WOODGRAIN Boston Oak Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "florentine_walnut": {
+    "type": "texture",
+    "name": "Florentine Walnut Woodmatt",
+    "thumbColor": "#8B5A2B",
+    "texture": "./assets/textures/WOODGRAIN Florentine Walnut Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "gloss_white": {
+    "type": "color",
+    "name": "Gloss White",
+    "thumbColor": "#ffffff",
+    "colorHex": "#F5F0EB",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.1
+  },
+  "matte_black_cab": {
+    "type": "color",
+    "name": "Matte Black",
+    "thumbColor": "#1a1a1a",
+    "colorHex": "#1A1A1A",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.7
+  },
+  "sage_green": {
+    "type": "color",
+    "name": "Sage Green",
+    "thumbColor": "#8DAF96",
+    "colorHex": "#8DAF96",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "navy_blue": {
+    "type": "color",
+    "name": "Navy Blue",
+    "thumbColor": "#2C3E6B",
+    "colorHex": "#2C3E6B",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "brushed_stainless": {
+    "type": "color",
+    "name": "Brushed Stainless",
+    "thumbColor": "#cccccc",
+    "colorHex": "#B0B8C1",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.3
+  },
+  "brushed_gold": {
+    "type": "color",
+    "name": "Brushed Gold",
+    "thumbColor": "#d4af37",
+    "colorHex": "#C49A3C",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.3
+  },
+  "matte_black_handle": {
+    "type": "color",
+    "name": "Matte Black",
+    "thumbColor": "#222222",
+    "colorHex": "#1C1C1C",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.5
+  },
+  "carrara_matte": {
+    "type": "color",
+    "name": "Carrara Matte",
+    "thumbColor": "#f0f0f0",
+    "colorHex": "#F5F5F5",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.5
+  },
+  "blanco_marquina": {
+    "type": "color",
+    "name": "Blanco Marquina",
+    "thumbColor": "#e6dfd8",
+    "colorHex": "#E6DFD8",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.5
+  },
+  "PAINT Black Satin": {
+    "type": "texture",
+    "name": "Black Satin",
+    "texture": "./assets/textures/PAINT Black Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Dark Grey Satin": {
+    "type": "texture",
+    "name": "Dark Grey Satin",
+    "texture": "./assets/textures/PAINT Dark Grey Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Grey Beige Satin": {
+    "type": "texture",
+    "name": "Grey Beige Satin",
+    "texture": "./assets/textures/PAINT Grey Beige Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Hogs Bristle Half": {
+    "type": "texture",
+    "name": "Hogs Bristle Half",
+    "texture": "./assets/textures/PAINT Hogs Bristle Half.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Light Grey Satin": {
+    "type": "texture",
+    "name": "Light Grey Satin",
+    "texture": "./assets/textures/PAINT Light Grey Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Light Pistachio Satin": {
+    "type": "texture",
+    "name": "Light Pistachio Satin",
+    "texture": "./assets/textures/PAINT Light Pistachio Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Navy Blue Satin": {
+    "type": "texture",
+    "name": "Navy Blue Satin",
+    "texture": "./assets/textures/PAINT Navy Blue Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT New Penny": {
+    "type": "texture",
+    "name": "New Penny",
+    "texture": "./assets/textures/PAINT New Penny.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Olive Satin": {
+    "type": "texture",
+    "name": "Olive Satin",
+    "texture": "./assets/textures/PAINT Olive Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Powder Pink": {
+    "type": "texture",
+    "name": "Powder Pink",
+    "texture": "./assets/textures/PAINT Powder Pink.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Steel Grey Satin": {
+    "type": "texture",
+    "name": "Steel Grey Satin",
+    "texture": "./assets/textures/PAINT Steel Grey Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT Taubmans Saxby Blue": {
+    "type": "texture",
+    "name": "Taubmans Saxby Blue",
+    "texture": "./assets/textures/PAINT Taubmans Saxby Blue.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT White Gloss": {
+    "type": "texture",
+    "name": "White Gloss",
+    "texture": "./assets/textures/PAINT White Gloss.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "PAINT White Satin": {
+    "type": "texture",
+    "name": "White Satin",
+    "texture": "./assets/textures/PAINT White Satin.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Aged Walnut AbsoluteGrain": {
+    "type": "texture",
+    "name": "Aged Walnut AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Aged Walnut AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Almond Granada 2440x1220": {
+    "type": "texture",
+    "name": "Almond Granada 2440x1220",
+    "texture": "./assets/textures/WOODGRAIN Almond Granada 2440x1220.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Australian Native Woodmatt": {
+    "type": "texture",
+    "name": "Australian Native Woodmatt",
+    "texture": "./assets/textures/WOODGRAIN Australian Native Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Blonde Oak Woodmatt": {
+    "type": "texture",
+    "name": "Blonde Oak Woodmatt",
+    "texture": "./assets/textures/WOODGRAIN Blonde Oak Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Boston Oak Woodmatt": {
+    "type": "texture",
+    "name": "Boston Oak Woodmatt",
+    "texture": "./assets/textures/WOODGRAIN Boston Oak Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Bottega Oak Woodmatt": {
+    "type": "texture",
+    "name": "Bottega Oak Woodmatt",
+    "texture": "./assets/textures/WOODGRAIN Bottega Oak Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Calm Oak AbsoluteGrain": {
+    "type": "texture",
+    "name": "Calm Oak AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Calm Oak AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Cannolo Cremona Oak": {
+    "type": "texture",
+    "name": "Cannolo Cremona Oak",
+    "texture": "./assets/textures/WOODGRAIN Cannolo Cremona Oak.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Classic Oak AbsoluteGrain": {
+    "type": "texture",
+    "name": "Classic Oak AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Classic Oak AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Coastal Oak Woodmatt": {
+    "type": "texture",
+    "name": "Coastal Oak Woodmatt",
+    "texture": "./assets/textures/WOODGRAIN Coastal Oak Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Cocoa Primavera Oak": {
+    "type": "texture",
+    "name": "Cocoa Primavera Oak",
+    "texture": "./assets/textures/WOODGRAIN Cocoa Primavera Oak.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Cotta Cremona Oak 2440x1220": {
+    "type": "texture",
+    "name": "Cotta Cremona Oak 2440x1220",
+    "texture": "./assets/textures/WOODGRAIN Cotta Cremona Oak 2440x1220.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Elegant Oak AbsoluteGrain": {
+    "type": "texture",
+    "name": "Elegant Oak AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Elegant Oak AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Florentine Walnut Woodmatt": {
+    "type": "texture",
+    "name": "Florentine Walnut Woodmatt",
+    "texture": "./assets/textures/WOODGRAIN Florentine Walnut Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Light Walnut AbsoluteGrain": {
+    "type": "texture",
+    "name": "Light Walnut AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Light Walnut AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Natural Walnut AbsoluteGrain": {
+    "type": "texture",
+    "name": "Natural Walnut AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Natural Walnut AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Notaio Walnut Woodmatt": {
+    "type": "texture",
+    "name": "Notaio Walnut Woodmatt",
+    "texture": "./assets/textures/WOODGRAIN Notaio Walnut Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Planked Urban Oak AbsoluteGrain": {
+    "type": "texture",
+    "name": "Planked Urban Oak AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Planked Urban Oak AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Prime Oak Woodmatt": {
+    "type": "texture",
+    "name": "Prime Oak Woodmatt",
+    "texture": "./assets/textures/WOODGRAIN Prime Oak Woodmatt.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Rural Oak AbsoluteGrain": {
+    "type": "texture",
+    "name": "Rural Oak AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Rural Oak AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Sepia Walnut AbsoluteGrain": {
+    "type": "texture",
+    "name": "Sepia Walnut AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Sepia Walnut AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Silk Primavera Oak 2440x1220": {
+    "type": "texture",
+    "name": "Silk Primavera Oak 2440x1220",
+    "texture": "./assets/textures/WOODGRAIN Silk Primavera Oak 2440x1220.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Smoked Kala Ash": {
+    "type": "texture",
+    "name": "Smoked Kala Ash",
+    "texture": "./assets/textures/WOODGRAIN Smoked Kala Ash.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Tassie Oak AbsoluteGrain": {
+    "type": "texture",
+    "name": "Tassie Oak AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Tassie Oak AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  },
+  "WOODGRAIN Victorian Ash Truescale AbsoluteGrain": {
+    "type": "texture",
+    "name": "Victorian Ash Truescale AbsoluteGrain",
+    "texture": "./assets/textures/WOODGRAIN Victorian Ash Truescale AbsoluteGrain.jpg",
+    "usdz": "./testmotion9.usdz",
+    "roughness": 0.8
+  }
+};
---
+++ js/products.js
@@ -0,0 +1,20 @@
+const CATEGORY_CONFIG = {
+    "handles": {
+        "Arch Pull": ["brushed_stainless", "brushed_gold", "matte_black_handle"]
+    },
+    "tops": {
+        "Same as Cabinet": [],
+        "SilkSurface": ["carrara_matte"]
+    }
+};
+
+const PRODUCT_CONFIG = {
+    // 这里的 KEY 是通过 URL ?id=XXX 传递的。如果没有传递，默认用这个。
+    "SOB-B-500-602": {
+        "mount": ["Floor Standing"],
+        "size": ["500 mm"],
+        "cabinet": ["boston_oak", "florentine_walnut", "PAINT Olive Satin", "PAINT Taubmans Saxby Blue", "PAINT New Penny", "PAINT White Satin"],
+        "handleCategory": ["Arch Pull"],
+        "topCategory": ["Same as Cabinet"]
+    }
+};
